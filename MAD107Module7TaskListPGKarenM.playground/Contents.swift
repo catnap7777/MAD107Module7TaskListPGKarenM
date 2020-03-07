b6dd49cb427295fc91ce2692eb34054d1f73fb5c
km <35552602+catnap7777@users.noreply.github.com>
2020-03-07 09:11:22 -0600

    sort on multiple tuple values with ternary operator

--- MAD107Module7TaskListPGKarenM.playground/Contents.swift
+++ MAD107Module7TaskListPGKarenM.playground/Contents.swift
@@ -20,32 +20,73 @@ var kamTaskList: [(task: String, priority: Int)] = [(task: "Do laundry", priorit
 var sortClosure1 = { ([(task: String, priority: Int)]) -> ([(task: String, priority: Int)]) in
     
     let kamArray = kamTaskList.sorted(by: {$0.priority < $1.priority })
+    
+    
+    //.. EXAMPLE of how to sort on multiple tuple values
+    var myArray = [ (1,1,"cat"), (1,2,"dog"), (3,4,"mouse99"), (3,4,"mouse1"), (3,4,"mouse2"), (3,1,"elephant"), (2,1,"hamster"), (2,2,"guinea pig")]
+    //myArray.sort{ $0.1 != $1.1 ? $0.1 > $1.1 : $0.0 < $1.0 } //.. use ternary operator -> (boolValue ? valueA : valueB) -> (boolValue ? true : false)
+    
+    myArray.sort{ $0.1 != $1.1 ? $0.1 < $1.1 : $0.2 < $1.2 }
+    print(myArray)
+    print("\n")
+    //[(1, 1, "cat"), (3, 1, "elephant"), (2, 1, "hamster"), (1, 2, "dog"), (2, 2, "guinea pig"), (3, 4, "mouse1"), (3, 4, "mouse2"), (3, 4, "mouse99")]
+    
+    
     return kamArray
+    
+    
+//.. original from stackoverflow
+//    var myArray = [ (1,1), (1,2), (3,4), (3,1), (2,1), (2,2)]
+//    //myArray.sort{ $0.1 != $1.1 ? $0.1 > $1.1 : $0.0 < $1.0 }
+//    myArray.sort{ $0.1 != $1.1 ? $0.1 < $1.1 : $0.0 < $1.0 }
+//    print(myArray)
+//    print("\n")
+//    // [(1, 2), (2, 2), (1, 1), (2, 1)]
+
+//.. another example from stackoverflow... but this one doesn't work
+//
+//    let sortedContacts = contacts.sort {
+//        if $0.lastName != $1.lastName { // first, compare by last names
+//            return $0.lastName < $1.lastName
+//        }
+//            /*  last names are the same, break ties by foo
+//             else if $0.foo != $1.foo {
+//             return $0.foo < $1.foo
+//             }
+//             ... repeat for all other fields in the sorting
+//             */
+//        else { // All other fields are tied, break ties by last name
+//            return $0.firstName < $1.firstName
+//        }
+//    }
+    
+  
+
 }
 
-func tasksForToday(maxTasks: Int, sortArray: ([(task: String, priority: Int)]) -> ([(task: String, priority: Int)])) {
+func tasksForToday(maxTasks: Int, sortClosure: ([(task: String, priority: Int)]) -> ([(task: String, priority: Int)])) {
     
     var count = 1
-    let done = sortArray(kamTaskList)
+    let doneArray = sortClosure(kamTaskList)
     
     var maxNbr = maxTasks
     
-    if maxNbr <= done.count {
+    if maxNbr <= doneArray.count {
         //
     } else {
-        maxNbr = done.count
+        maxNbr = doneArray.count
     }
     
-    print("   Number of tasks printed: \(maxNbr) out of \(done.count)")    //"\(maxTasks)")
+    print("   Number of tasks printed: \(maxNbr) out of \(doneArray.count)")    //"\(maxTasks)")
     print("\n Priority             Task      ")
     print(" --------       ----------------")
 
-    for item in done where (count <= maxNbr && maxNbr <= done.count) {
+    for item in doneArray where (count <= maxNbr && maxNbr <= doneArray.count) {
         print("    \(item.priority)    \t\t\(item.task)")
         count += 1
     }
 }
 
-tasksForToday(maxTasks: 12, sortArray: sortClosure1)
+tasksForToday(maxTasks: 12, sortClosure: sortClosure1)
 
 
